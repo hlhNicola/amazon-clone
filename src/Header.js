6ebf1ca5812ea0a9aebf9baa1391d1024d3456fd
hlhNicola <littlefly14@gmail.com>
2020-12-16 14:56:31 -0600

    create checkout components and set up initial data layer

--- src/Header.js
+++ src/Header.js
@@ -2,12 +2,16 @@ import React from 'react';
 import './Header.css';
 import SearchIcon from '@material-ui/icons/Search';
 import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+import { Link } from 'react-router-dom';
 
 
 function Header() {
     return (
         <div className='header'>
-            <img className='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+            <Link to='/'>
+                <img className='header_logo' src='http://pngimg.com/uploads/amazon/amazon_PNG11.png' />
+            </Link>
+           
             <div className="header_search" type='text'>
                 <input type="text" className="header_searchInput"/>
                 <SearchIcon className="header_searchIcon"></SearchIcon>
@@ -39,11 +43,12 @@ function Header() {
                     </span>
                 
                 </div>
-
-                <div className="header_optionBasket">
-                    <ShoppingBasketIcon />
-                    <span className="header_optionLineTwo header_basketCount">0</span>
-                </div>
+                <Link to="/checkout">
+                    <div className="header_optionBasket">
+                        <ShoppingBasketIcon />
+                        <span className="header_optionLineTwo header_basketCount">0</span>
+                    </div>
+                </Link>
             </div>
         </div>
           
