f9ce620a7ca178192e7cd7bee81bca80c3369bb3
hlhNicola <littlefly14@gmail.com>
2020-12-16 19:27:19 -0600

    working on dispatch functions, try to delete the item from basket

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,34 @@
+import React from 'react'
+import './CheckoutProduct.css'
+import StarIcon from '@material-ui/icons/Star';
+import { useStateValue } from './StateProvider';
+
+function CheckoutProduct({id,image, title, price, rating}) {
+    const [{basket}, dispatch] = useStateValue()
+
+    const removeFromBasket = (id, basket) => {
+        dispatch({
+            type: 'REMOVE_FROM_BASKET',
+            id: id,
+        })
+    }
+    return (
+        <div className='checkoutProduct'>
+            <img src={image} className='checkoutProduct_image' alt=""/>
+            <div className="checkoutProduct_info">
+                <p className="checkoutProduct_title">{title}</p>
+                <p className="checkoutProduct_price">
+                    <small>$</small>
+                    <strong>{price}</strong>
+                </p>
+                <div className="checkoutProduct_rating">
+                    {Array(rating).fill().map((item, index)=>(<StarIcon className="star_icon"/>))}
+                </div>
+                <button onClick={removeFromBasket(basket)}>Remove from Basket</button>
+            </div>
+        </div>
+        
+    )
+}
+
+export default CheckoutProduct
