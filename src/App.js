3b53c460b86c2f8a15d83423559b4d5edf0ef57a
hlhNicola <littlefly14@gmail.com>
2021-02-24 15:30:35 -0600

    add authentication

--- src/App.js
+++ src/App.js
@@ -4,8 +4,33 @@ import Home from './Home';
 import Checkout from './Checkout';
 import Login from './Login';
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import React, { useEffect } from "react";
+import { auth } from './firebase';
+import {useStateValue} from './StateProvider';
 
 function App() {
+
+const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // only run once when the app component loads
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+      if(authUser){
+        // the user just logged in / the user was logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+      } else {
+        // the user is logged in
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, [])
   return (
     <Router>
       <div className="app">
