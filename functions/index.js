d899c81090e3642d848517c4e82b5d909c5d0c9f
hlhNicola <littlefly14@gmail.com>
2021-03-03 03:27:47 -0600

    add express backend

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,17 @@
 const functions = require('firebase-functions');
+const express = require('express');
+const cors = require('cors')
+const stripe = require("stripe")(
+    'sk_test_51IQoQVEQJlG7DRevZIFu28YKtMlg9p5LWfqr66Fplzyu7rs9JFqI3x7VJb0xpaBD7rkiSRwHtoGQPrxDElHC9wdC00eBfzGV9m')
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const app = express()
+
+app.use(cors({origin: true}))
+app.use(express.json())
+
+
+app.get('/', (request, response) => response.status(200).send('hello world'))
+
+exports.api = functions.https.onRequest(app)
+
+// http://localhost:5001/clone-eebc3/us-central1/api
