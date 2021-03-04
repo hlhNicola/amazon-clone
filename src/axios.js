20a96d3bcfda117e38feb1cc555867921a900620
hlhNicola <littlefly14@gmail.com>
2021-03-04 15:32:29 -0600

    deploy to firebase

--- src/axios.js
+++ src/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:5001/clone-eebc3/us-central1/api' 
+    // baseURL: 'http://localhost:5001/clone-eebc3/us-central1/api'  // local
+    baseURL: 'https://us-central1-clone-eebc3.cloudfunctions.net/api'
+
 });
 
 export default instance
